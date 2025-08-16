209a31393c865c0b138198c242404597ae352239
greddode <greddodeash@gmail.com>
2025-08-16 10:15:50 -0500

    Added Mouse Lock

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,4 +1,4 @@
 pub mod game;
-pub mod window;
 pub mod level;
 pub mod player;
+pub mod window;
--- src/game/window/window.rs
+++ src/game/window/window.rs
@@ -1,17 +1,48 @@
 use bevy::{
     prelude::*,
-    window::{PrimaryWindow, WindowMode, WindowResolution},
+    window::{CursorGrabMode, CursorOptions, PrimaryWindow, WindowMode, WindowResolution},
 };
 
 pub struct WindowSettingsPlugin;
 impl Plugin for WindowSettingsPlugin {
     fn build(&self, app: &mut App) {
-            app.add_systems(PreStartup, init_window);
+        app
+            .add_systems(PreStartup, init_window)
+            .add_systems(Startup, cursor_lock)
+            .add_systems(Update, grab_mouse);
     }
 }
+
 fn init_window(mut window_query: Query<&mut Window, With<PrimaryWindow>>) {
     if let Ok(mut window) = window_query.single_mut() {
         window.resolution = WindowResolution::new(1920., 1080.);
         window.mode = WindowMode::BorderlessFullscreen(MonitorSelection::Current);
     }
+}
+
+fn cursor_lock()
+{
+    let window = Window {
+        cursor_options: CursorOptions {
+            grab_mode: CursorGrabMode::Confined,
+            ..default()
+        },
+        ..default()
+    };
+}
+fn grab_mouse(
+    mut window: Single<&mut Window>,
+    mouse: Res<ButtonInput<MouseButton>>,
+    key: Res<ButtonInput<KeyCode>>,
+)
+{
+    if mouse.just_pressed(MouseButton::Left) {
+        window.cursor_options.visible = false;
+        window.cursor_options.grab_mode = CursorGrabMode::Locked;
+    }
+    if key.just_pressed(KeyCode::Escape) {
+        window.cursor_options.visible = true;
+        window.cursor_options.grab_mode = CursorGrabMode::None;
+    }
+
 }
